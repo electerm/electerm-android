c215d0d30d233bfee4561dd74917ac59b5248250
ZHAO Xudong <zxdong@gmail.com>
2026-08-09 15:55:44 +0800

    FIx [r2]

--- build/android/res-overlay/java/org/electerm/electerm/MainActivity.java
+++ build/android/res-overlay/java/org/electerm/electerm/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
         applySafeAreaPadding();
