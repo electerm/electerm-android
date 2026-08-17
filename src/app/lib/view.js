dcc04741c95922022a2faa863885701be8c29f53
ZHAO Xudong <zxdong@gmail.com>
2026-08-17 09:12:13 +0800

    Remove unused

--- src/app/lib/view.js
+++ src/app/lib/view.js
@@ -29,56 +29,14 @@ const defaultAIPreset = {
   nameAI: 'ai.electerm.org(default free)'
 }
 
-let needMigrate
-
 function buildServer () {
   return `http://${process.env.HOST}:${process.env.PORT}`
-}
-
-function checkNeedMigrate () {
-  if (needMigrate !== undefined) {
-    return needMigrate
-  }
-
-  const nedbPath = process.env.DB_PATH || resolve(cwd, 'data/nedb-database')
-  const nedbUserPath = resolve(nedbPath, 'users', defaultUserName)
-
-  // Check if nedb directory exists and has .nedb files
-  if (fs.existsSync(nedbUserPath)) {
-    const nedbFiles = fs.readdirSync(nedbUserPath).filter(file => file.endsWith('.nedb'))
-
-    if (nedbFiles.length > 0) {
-      needMigrate = true
-      return needMigrate
-    }
-  }
-
-  needMigrate = false
-  return needMigrate
-}
-
-async function checkNodePty () {
-  if (process.env.DISABLE_LOCAL_TERMINAL) {
-    return false
-  }
-  return import('node-pty')
-    .then(() => true)
-    .catch(() => false)
 }
 
 export async function index (req, res) {
   const server = process.env.SERVER || (isDev ? buildServer() : '')
   const cdn = process.env.CDN || server
-  const hasNodePty = await checkNodePty()
-  const needMigrate = checkNeedMigrate()
-  if (needMigrate) {
-    migrationNotice(
-      'electerm-web v3',
-      'nedb',
-      'sqlite',
-      'electerm-data-tool --data-path "/path/to/data/nedb-database" export data.json'
-    )
-  }
+  const hasNodePty = false
   // All session types the app knows about.
   const supportSessionTypes = [
     'ssh',
@@ -109,7 +67,7 @@ export async function index (req, res) {
     query: req.query,
     server,
     hasNodePty,
-    needMigrate,
+    needMigrate: false,
     supportSessionTypes
   }
   const {
